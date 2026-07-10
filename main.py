6f83f8c82093d523d9d74f98cc26745e0fe831ab
Evelyn <evelyn.ciara.345@gmail.com>
2026-07-10 01:24:40 -0400

    Change audio and bg_image to positional args

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def parse_args():
         epilog="Happy podcasting!",
     )
     parser.add_argument(
-        "--audio",
+        "audio",
         help="input audio or video file (if a video file is provided, only the audio track will be used)",
     )
-    parser.add_argument("--bg", help="background image file")
+    parser.add_argument("bg_image", help="background image file")
     parser.add_argument("-o", "--output", help="output video file")
     parser.add_argument(
         "--vc",
@@ -32,6 +32,12 @@ def parse_args():
 
 def main():
     args = parse_args()
+    audio_file_path = args.audio
+    image_file_path = args.bg_image
+    print(f"Audio file: {audio_file_path or '[none]'}")
+    print(f"Image file: {image_file_path or '[none]'}")
+    output_file_path = args.output
+    print(f"Video will be written to: {output_file_path}")
 
 
 if __name__ == "__main__":
