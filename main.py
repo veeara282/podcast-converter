8390cf946aa705c3ef6ae238b87394294b78ceee
Evelyn <evelyn.ciara.345@gmail.com>
2026-07-10 00:12:54 -0400

    format with black

--- main.py
+++ main.py
@@ -7,17 +7,26 @@ def parse_args():
         description="Converts podcast audio to video",
         epilog="Happy podcasting!",
     )
-    parser.add_argument("--audio", help="input audio or video file (if a video file is provided, only the audio track will be used)")
+    parser.add_argument(
+        "--audio",
+        help="input audio or video file (if a video file is provided, only the audio track will be used)",
+    )
     parser.add_argument("--bg", help="background image file")
     parser.add_argument("-o", "--output", help="output video file")
-    parser.add_argument("--vc", "--video-codec",
-                        help="video encoding to use for the output",
-                        choices=["h264", "hevc", "vp9", "av1"],
-                        default="av1")
-    parser.add_argument("--ac", "--audio-codec",
-                        help="audio encoding to use for the output",
-                        choices=["aac", "opus"],
-                        default="opus")
+    parser.add_argument(
+        "--vc",
+        "--video-codec",
+        help="video encoding to use for the output",
+        choices=["h264", "hevc", "vp9", "av1"],
+        default="av1",
+    )
+    parser.add_argument(
+        "--ac",
+        "--audio-codec",
+        help="audio encoding to use for the output",
+        choices=["aac", "opus"],
+        default="opus",
+    )
     return parser.parse_args()
 
 
