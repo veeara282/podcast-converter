33b4a92f440423ebdbc6e0cf25234892718101ea
Evelyn <evelyn.ciara.345@gmail.com>
2026-07-15 16:03:44 -0400

    Compute bar heights in PyTorch

--- visualizer.py
+++ visualizer.py
@@ -2,6 +2,7 @@ import logging
 
 import numpy as np
 import skia
+import torch
 from torch import Tensor
 
 logger = logging.getLogger()
@@ -17,7 +18,7 @@ def draw_background(surface: skia.Surface) -> None:
         canvas.drawRect(rect_bg, paint_bg)
 
 
-def draw_frame(spectrograms: Tensor, t: int) -> np.ndarray:
+def draw_frame(bar_heights: Tensor, t: int) -> np.ndarray:
     surface = skia.Surface(1080, 1080)
     # First draw the background
     draw_background(surface)
@@ -30,16 +31,15 @@ def draw_frame(spectrograms: Tensor, t: int) -> np.ndarray:
             Color=skia.ColorWHITE,
             Style=skia.Paint.kFill_Style,
         )
-        n_mels = spectrograms.size(1)
+        n_mels = bar_heights.size(1)
         bar_width = 12
         gap_width = 4
         leftmost = 27
         for i in range(n_mels):
-            db = spectrograms[0, i, t]
-            bar_height = max(0, db + 80) * 8
+            bar_height = bar_heights[0, i, t]
             x_left = leftmost + i * (bar_width + gap_width)
             x_right = x_left + bar_width
-            y_bottom = 960
+            y_bottom = 900
             y_top = y_bottom - bar_height
             rect = skia.Rect.MakeLTRB(x_left, y_top, x_right, y_bottom)
             canvas.drawRect(rect, paint)
@@ -47,9 +47,18 @@ def draw_frame(spectrograms: Tensor, t: int) -> np.ndarray:
     return surface.makeImageSnapshot().toarray()
 
 
+def spectrograms_to_bar_heights(spectrograms: Tensor, max_height=720) -> Tensor:
+    # No need to clip the dynamic range - AmplitudeToDB already clips it to [-100, 0] dB
+    # Normalize the dynamic range [min_db, 0] -> [0, 1]
+    db_range = -torch.amin(spectrograms)
+    normalized = spectrograms / db_range + 1
+    return normalized * max_height
+
+
 def draw_frames(spectrograms: Tensor):
+    bar_heights = spectrograms_to_bar_heights(spectrograms)
     # Use a generator to draw frames so we can stream them to the video encoder.
     n_frames = spectrograms.size(2)
     for t in range(n_frames):
         logger.debug(f"Drawing frame {t} of {n_frames}...")
-        yield draw_frame(spectrograms, t)
+        yield draw_frame(bar_heights, t)
