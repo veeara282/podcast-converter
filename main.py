31da1a904e86676b08cd2ce822c98c985cb58b2a
Evelyn <evelyn.ciara.345@gmail.com>
2026-07-16 14:44:57 -0400

    Mark unused args; add vorbis as codec option

--- main.py
+++ main.py
@@ -19,28 +19,28 @@ def parse_args():
         "audio",
         help="input audio or video file (if a video file is provided, only the audio track will be used)",
     )
-    parser.add_argument("-b", "--bg-image", help="background image file")
     parser.add_argument("-o", "--output", help="output video file")
-    parser.add_argument(
-        "--vc",
-        "--video-codec",
-        help="video encoding to use for the output",
-        choices=["h264", "hevc", "vp9", "av1"],
-        default="av1",
-    )
-    parser.add_argument(
-        "--ac",
-        "--audio-codec",
-        help="audio encoding to use for the output",
-        choices=["aac", "opus"],
-        default="opus",
-    )
     parser.add_argument(
         "-f",
         "--frame-rate",
         help="the desired video frame rate in Hz. Default: 30",
         type=int,
         default=30,
+    )
+    parser.add_argument("-b", "--bg-image", help="background image file (currently unused)")
+    parser.add_argument(
+        "--vc",
+        "--video-codec",
+        help="video encoding to use for the output (currently unused)",
+        choices=["h264", "hevc", "vp9", "av1"],
+        default="av1",
+    )
+    parser.add_argument(
+        "--ac",
+        "--audio-codec",
+        help="audio encoding to use for the output (currently unused)",
+        choices=["aac", "opus", "vorbis"],
+        default="opus",
     )
     return parser.parse_args()
 
