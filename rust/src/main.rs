b662805cc56e984d1353ec4e4f38b532d7e3389a
Evelyn <evelyn.ciara.345@gmail.com>
2026-08-13 14:35:46 -0400

    Parse args

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,35 @@
+use clap::Parser;
+
+/// Converts podcast audio to video
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// input audio or video file (if a video file is provided, only the audio track will be used)
+    #[arg(short, long)]
+    audio: String,
+
+    /// output video file
+    #[arg(short, long)]
+    output: String,
+
+    /// the desired video frame rate in Hz
+    #[arg(short, long, default_value_t = 30)]
+    frame_rate: u16,
+
+    /// video width in pixels
+    #[arg(short, long, default_value_t = 1080)]
+    width: u16,
+
+    /// video height in pixels
+    #[arg(short='y', long, default_value_t = 1080)]
+    height: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Audio file: {}", args.audio);
+    println!("Frame rate: {}", args.frame_rate);
+    println!("Dimensions: {} x {}", args.width, args.height);
+    println!("Video will be written to: {}", args.output);
 }
